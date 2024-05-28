b195a40e464f456f7eeae70e6626527f119bd7d1
douglasfnascimento <douglasfnascimento@icloud.com>
2024-05-27 23:31:57 -0300

    ajustado menu mobile e menus viraram fixos

--- js/script.js
+++ js/script.js
@@ -1,4 +1,13 @@
 const hamburguer = document.querySelector(".hamburguer");
 const nav = document.querySelector(".nav");
+const menuItems = document.querySelectorAll(".nav-list a");
 
-hamburguer.addEventListener("click", () => nav.classList.toggle("active"));
+hamburguer.addEventListener("click", () => {
+    nav.classList.toggle("active");
+});
+
+menuItems.forEach(item => {
+    item.addEventListener("click", () => {
+        nav.classList.remove("active");
+    });
+});
